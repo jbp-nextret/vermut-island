27a765a0099ba9bafe5766c67ec66b9c21737324
jbp-nextret <jbp@nextret.net>
2026-07-08 15:22:18 +0200

    #4 Proves shaders

--- Scripts/Harvest/seed_data.gd
+++
@@ -1,8 +0,0 @@
-class_name SeedData
-extends Resource
-
-@export var nom: String = "Romaní"
-@export var grow_days: int = 5
-@export var rendiment_base: int = 3
-@export var estacions: Array[String] = ["primavera", "estiu"]
-@export var sprite_frames: SpriteFrames  # referència als frames de creixement
